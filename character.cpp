fec9e347d4ee90d3c5c636b33f6cb24e6389f394
Calvin Julianza Rindy Hartanto <163235006+Vinza15@users.noreply.github.com>
2025-04-10 23:59:43 +0700

    Delete character.cpp

--- character.cpp
+++
@@ -1,6 +0,0 @@
-#include "Character.h"
-
-Character::Character(int startX, int startY) : x(startX), y(startY) {}
-
-int Character::getX() const { return x; }
-int Character::getY() const { return y; }
